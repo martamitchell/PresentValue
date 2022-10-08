fc057d18a30b3b99fa20a78173ebd7e2745cec84
marta <marta@DESKTOP-5IHFULR>
2022-10-08 17:13:19 -0400

    Add project files.

---
+++ PresentValue/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿// Group 4 Author: Marta Group Members: Aman, Himanshu, Srivani, Meet, Robin
+using System.Windows;
+
+namespace PresentValue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        VM vm = new VM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.UpdatePresentValue();
+        }
+
+    }
+
+}
---
+++ PresentValue/VM.cs
@@ -0,0 +1,63 @@
+﻿// Group 4 Author: Marta Group Members: Aman, Himanshu, Srivani, Meet, Robin
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PresentValue
+{
+    internal class VM : INotifyPropertyChanged
+    {
+        const decimal FUTURE_VALUE = 10000m;
+        const double YEARS = 10d;
+
+        private decimal interest = 0;
+
+        public decimal Interest
+        {
+            get { return interest; }
+            set { interest = value; notifyChange(); UpdatePresentValue(); }
+        }
+
+        private double years = YEARS;
+
+        public double Years
+        {
+            get { return years; }
+            set { years = value; notifyChange(); UpdatePresentValue(); }
+        }
+
+        private decimal presentValue = 0;
+        public decimal PresentValue
+        {
+            get { return presentValue; }
+            set { presentValue = value; notifyChange(); }
+        }
+
+        private decimal futureValue = FUTURE_VALUE;
+        public decimal FutureValue
+        {
+            get { return futureValue; }
+            set { futureValue = value; notifyChange(); UpdatePresentValue(); }
+        }
+
+        public void UpdatePresentValue()
+        {
+            PresentValue = CalcPresentValue(futureValue, interest, years);
+            //alternative: PresentValue = future / Math.Pow(1 + interestRate, numYears);
+        }
+
+        static public decimal CalcPresentValue(decimal future, decimal interestRate, double numYears)
+        {
+            return future / (decimal)Math.Pow((double)(1 + interestRate), numYears);
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void notifyChange([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
